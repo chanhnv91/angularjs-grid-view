803fdc8047bbb6f371f6c73a0f123620cb80daa0
chanhnguyen <chanhnv@nexlesoft.com>
2021-07-14 23:16:39 -0700

    no message

--- release/dataTable.js
+++ release/dataTable.js
@@ -431,17 +431,19 @@
       isSafari = /Safari\//.test(ua) && !/Chrome\//.test(ua);
 
   function TranslateXY(styles, x, y) {
-    if (hasCSSTransforms) {
-      if (!isSafari && hasCSS3DTransforms) {
-        styles[transform] = "translate3d(" + x + "px, " + y + "px, 0)";
-        styles[backfaceVisibility] = 'hidden';
-      } else {
-        styles[CamelCase(transform)] = "translate(" + x + "px, " + y + "px)";
-      }
-    } else {
-      styles.top = y + 'px';
-      styles.left = x + 'px';
-    }
+    // if (hasCSSTransforms) {
+    //   if (!isSafari && hasCSS3DTransforms) {
+    //     styles[transform] = "translate3d(" + x + "px, " + y + "px, 0)";
+    //     styles[backfaceVisibility] = 'hidden';
+    //   } else {
+    //     styles[CamelCase(transform)] = "translate(" + x + "px, " + y + "px)";
+    //   }
+    // } else {
+    //   styles.top = y + 'px';
+    //   styles.left = x + 'px';
+    // }
+    styles.top = y + 'px';
+    styles.left = x + 'px';
   }
 
   var GroupRowController = function () {
